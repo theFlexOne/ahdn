18532f2e3ba4a49e9236c2af533570cd57c1ae4c
flexone <edhavey@gmail.com>
2026-03-20 16:35:48 -0500

    Replace favicon.ico with custom SVG favicon

--- src/root.tsx
+++ src/root.tsx
@@ -24,7 +24,7 @@ function Document({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
         <Meta />
         <Links />
       </head>
